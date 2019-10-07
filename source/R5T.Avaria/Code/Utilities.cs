c26995818eb4ef9a4f38cc9c47b0346056e1adac
MinexAutomation <minex.automation@gmail.com>
2019-10-06 22:15:19 -0700

    Added Virconium functionality (ability to remove extraneous dependencies, and add missing dependencies).

---
+++ source/R5T.Avaria/Code/Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+using VirconiumUtilities = R5T.Private.Virconium.Lib.Utilities;
+
+
+namespace R5T.Avaria
+{
+    public static class Utilities
+    {
+        public static void RemoveExtraneousDependencies(string solutionFilePath, string[] projectFilePaths, TextWriter writer, bool dryRun = true)
+        {
+            VirconiumUtilities.RemoveExtraneousDependencies(solutionFilePath, projectFilePaths, writer, dryRun);
+        }
+
+        public static void RemoveExtraneousDependencies(string solutionFilePath, string[] projectFilePaths, bool dryRun = true)
+        {
+            VirconiumUtilities.RemoveExtraneousDependencies(solutionFilePath, projectFilePaths, dryRun);
+        }
+
+        public static void AddMissingDependencies(string solutionFilePath, TextWriter writer, bool dryRun = true)
+        {
+            VirconiumUtilities.AddMissingDependencies(solutionFilePath, writer, dryRun);
+        }
+
+        public static void AddMissingDependencies(string solutionFilePath, bool dryRun = true)
+        {
+            VirconiumUtilities.AddMissingDependencies(solutionFilePath, dryRun);
+        }
+    }
+}
