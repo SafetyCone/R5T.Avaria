25a30c6dac77e805168b19752300c32e899a9615
MinexAutomation <minex.automation@gmail.com>
2019-10-07 23:32:15 -0700

    Added functionality to add missing dependencies of, and remove extraneous dependencies of, the solution file a maintenance project referencing the Avaria library is part of.

--- source/R5T.Avaria.Maintenance/Code/Program.cs
+++ source/R5T.Avaria.Maintenance/Code/Program.cs
@@ -1,19 +1,53 @@
 ﻿using System;
 
+using AvariaUtilities = R5T.Avaria.Utilities;
+
+
 namespace R5T.Avaria.Maintenance
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Construction.SubMain();
+            //Construction.SubMain();
 
-            //Program.SubMain();
+            Program.SubMain();
         }
 
         private static void SubMain()
         {
-            Console.WriteLine("Hello World!");
+            //Program.RemoveExtraneousDependencies(true);
+            Program.AddMissingDependencies(true);
+        }
+
+        private static void RemoveExtraneousDependencies(bool dryRun)
+        {
+            var currentExecutableFilePath = AvariaUtilities.GetExecutablePath();
+
+            Console.WriteLine($"Current executable file path:\n{currentExecutableFilePath}");
+
+            var solutionFilePathProvider = new DevelopmentSolutionFilePathProvider();
+
+            var solutionFilePath = solutionFilePathProvider.GetSolutionFilePath(currentExecutableFilePath, @"R5T.Avaria.sln");
+
+            Console.WriteLine($"Solution file path:\n{solutionFilePath}");
+
+            AvariaUtilities.RemoveExtraneousDependencies(solutionFilePath, dryRun);
+        }
+
+        private static void AddMissingDependencies(bool dryRun)
+        {
+            var currentExecutableFilePath = AvariaUtilities.GetExecutablePath();
+
+            Console.WriteLine($"Current executable file path:\n{currentExecutableFilePath}");
+
+            var solutionFilePathProvider = new DevelopmentSolutionFilePathProvider();
+
+            var solutionFilePath = solutionFilePathProvider.GetSolutionFilePath(currentExecutableFilePath, @"R5T.Avaria.sln");
+
+            Console.WriteLine($"Solution file path:\n{solutionFilePath}");
+
+            AvariaUtilities.AddMissingDependencies(solutionFilePath, dryRun);
         }
     }
 }
--- source/R5T.Avaria/Code/Utilities.cs
+++ source/R5T.Avaria/Code/Utilities.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 
+using BasePathUtilities = R5T.NetStandard.IO.Paths.Base.Utilities;
 using VirconiumUtilities = R5T.Private.Virconium.Lib.Utilities;
 
 
@@ -8,6 +9,12 @@ namespace R5T.Avaria
 {
     public static class Utilities
     {
+        public static string GetExecutablePath()
+        {
+            var executablePath = BasePathUtilities.ExecutablePathValue;
+            return executablePath;
+        }
+
         public static void RemoveExtraneousDependencies(string solutionFilePath, string[] projectFilePaths, TextWriter writer, bool dryRun = true)
         {
             VirconiumUtilities.RemoveExtraneousDependencies(solutionFilePath, projectFilePaths, writer, dryRun);
@@ -16,6 +23,16 @@ namespace R5T.Avaria
         public static void RemoveExtraneousDependencies(string solutionFilePath, string[] projectFilePaths, bool dryRun = true)
         {
             VirconiumUtilities.RemoveExtraneousDependencies(solutionFilePath, projectFilePaths, dryRun);
+        }
+
+        public static void RemoveExtraneousDependencies(string solutionFilePath, TextWriter writer, bool dryRun = true)
+        {
+            VirconiumUtilities.RemoveExtraneousDependencies(solutionFilePath, writer, dryRun);
+        }
+
+        public static void RemoveExtraneousDependencies(string solutionFilePath, bool dryRun = true)
+        {
+            VirconiumUtilities.RemoveExtraneousDependencies(solutionFilePath, dryRun);
         }
 
         public static void AddMissingDependencies(string solutionFilePath, TextWriter writer, bool dryRun = true)
